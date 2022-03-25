8387d08e62bb730c6803e4368b6b96a88a467396
EricSylvestre <ericsylvestre19@gmail.com>
2022-03-24 23:42:57 -0400

    update

--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,4 +1,4 @@
-import React, {useEffect } from 'react';
+import {useEffect } from 'react';
 import { useAuth } from '../contexts/AuthProvider'
 import { getFirestore, getDocs, collection } from 'firebase/firestore'
 
